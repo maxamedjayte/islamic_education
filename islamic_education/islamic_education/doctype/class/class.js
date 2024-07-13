c665f47063f09cdf8362423547b67d503be186f0
Maxamed Jayte <maxamedltc@gmail.com>
2024-07-13 13:12:20 -0400

    Update class_enrolment.json and class_enrolment.py files

--- islamic_education/islamic_education/doctype/class/class.js
+++ islamic_education/islamic_education/doctype/class/class.js
@@ -1,27 +1,18 @@
 // Copyright (c) 2024, Maxamed Jayte and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on('Class', {
-// 	refresh: function(frm) {
-// 		// filter the academic year based on the selected academic term
-// 		frm.set_query("academic_year", function() {
-// 			return {
-// 				"filters": {
-// 					"active_academic_year": 1
-// 				}
-// 			};
-// 		}
-// 	);
-// 	}
-// });
+frappe.ui.form.on('Class', {
+	refresh: function(frm) {
+		// filter the academic year based on the selected academic term
+	// 	frm.set_query("academic_year", function() {
+	// 		return {
+	// 			"filters": {
+	// 				"active_academic_year": 1
+	// 			}
+	// 		};
+	// 	}
+	// );
+	}
+});
 
 
-// frappe.listview_settings['Class'] = {
-// 	add_fields: [],
-// 	onload: function(listview) {
-// 		frappe.route_options = {
-// 			"academic_year": "1444%20-%201445"
-// 		};
-// 		listview.refresh( );  
-// 	}
-// };
