4e15305e0b746da80bd115f5313f23e93c03d97f
Maxamed Jayte <maxamedltc@gmail.com>
2024-07-29 03:19:34 -0400

    Update Islamic Education module with new files and modifications to existing files

--- islamic_education/islamic_education/doctype/class_enrolment/test_class_enrolment.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Maxamed Jayte and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestClassEnrolment(FrappeTestCase):
-	pass
