af250877c31cdcc7edaf11d3c9caa4a5debc2f03
Maxamed Jayte <maxamedltc@gmail.com>
2024-07-29 05:25:51 -0400

    Fix variable name in StudentClassEnrolment model

--- islamic_education/islamic_education/doctype/class_transfer/class_transfer.py
+++ islamic_education/islamic_education/doctype/class_transfer/class_transfer.py
@@ -6,42 +6,53 @@ class ClassTransfer(Document):
         from_class = frappe.get_doc("Class", self.from_class)
         to_class = frappe.get_doc("Class", self.to_class)
         
+        # Ensure transfer only happens if academic years are different
         if from_class.academic_year != to_class.academic_year:
             if self.transfer_type == "By Student":
-                # Properly indented to align with the if statement
-                students = frappe.get_list("Student", filters={"classe": from_class.name}, fields=["name"])
-                for std in students:
-                    if not std.status == "Active":
-                        pass
-                    enrolment = frappe.get_list("Class Enrolment", filters={"student": std.name, "current_class": from_class.name}, fields=["name"])
-                    if not enrolment:
-                        new_enrolment = frappe.new_doc("Class Enrolment")
-                        new_enrolment.student = std.name
-                        new_enrolment.current_classe = from_class.name
-                        new_enrolment.enrolment_date = from_class.registered_date
-                        new_enrolment.save()
-                    
-                    to_class_enrolment = frappe.get_list("Class Enrolment", filters={"student": std.name, "current_class": to_class.name}, fields=["name"])
-                    if not to_class_enrolment:
-                        new_enrolment = frappe.new_doc("Class Enrolment")
-                        new_enrolment.student = std.name
-                        new_enrolment.current_classe = to_class.name
-                        new_enrolment.enrolment_date = to_class.registered_date
-                        new_enrolment.save()
-
-                        frappe.db.set_value("Student", std.name, "classe", to_class.name)
-                        frappe.db.commit()
+                students = frappe.get_list("Student", filters={"classe": from_class.name}, fields=["name", "status"])
+                for student in students:
+                    if student.status == "Active":
+                        # Check existing enrollment in from_class
+                        enrolment = frappe.get_list("Student Class Enrolment", filters={
+                            "student": student.name,
+                            "current_class": from_class.name
+                        }, fields=["name"])
+                        if not enrolment:
+                            # Create enrolment in from_class if it doesn't exist
+                            new_enrolment = frappe.new_doc("Student Class Enrolment")
+                            new_enrolment.student = student.name
+                            new_enrolment.current_class = from_class.name
+                            new_enrolment.enrolment_date = from_class.registered_date
+                            new_enrolment.save()
+                        
+                        # Check existing enrolment in to_class
+                        to_class_enrolment = frappe.get_list("Student Class Enrolment", filters={
+                            "student": student.name,
+                            "current_class": to_class.name
+                        }, fields=["name"])
+                        if not to_class_enrolment:
+                            # Create enrolment in to_class if it doesn't exist
+                            new_to_enrolment = frappe.new_doc("Student Class Enrolment")
+                            new_to_enrolment.student = student.name
+                            new_to_enrolment.current_class = to_class.name
+                            new_to_enrolment.enrolment_date = to_class.registered_date
+                            new_to_enrolment.save()
+                            
+                            # Update student's class
+                            frappe.db.set_value("Student", student.name, "classe", to_class.name)
             else:
-                # If the transfer_type is not "By Student"
-                from_enrolment_students = frappe.get_list("Class Enrolment", filters={"current_class": from_class.name}, fields=["student"])
-                for std in from_enrolment_students:
-                    if not std.status:
-                        std.status = "Active"
-                    enrolment = frappe.get_list("Class Enrolment", filters={"student": std.student, "current_class": to_class.name}, fields=["name"])
-                    if not enrolment:
-                        new_enrolment = frappe.new_doc("Class Enrolment")
-                        new_enrolment.student = std.student
-                        new_enrolment.classe = to_class.name
+                # Handle other transfer types (e.g., "By Enrolment")
+                from_enrolment_students = frappe.get_list("Student Class Enrolment", filters={
+                    "current_class": from_class.name
+                }, fields=["student"])
+                for enrolment in from_enrolment_students:
+                    # Check and create enrolment in to_class
+                    if not frappe.get_value("Student Class Enrolment", {"student": enrolment.student, "current_class": to_class.name}, "name"):
+                        new_enrolment = frappe.new_doc("Student Class Enrolment")
+                        new_enrolment.student = enrolment.student
+                        new_enrolment.current_class = to_class.name
                         new_enrolment.enrolment_date = to_class.registered_date
                         new_enrolment.save()
-                        frappe.db.set_value("Student", std.student, "classe", to_class.name)
+                        frappe.db.set_value("Student", enrolment.student, "classe", to_class.name)
+
+        frappe.db.commit()  # Ensure all database transactions are committed after processing
